a25519af5c97c1b08846822494da8414fc2edeb6
Petar Kapulica <petarkapulica@hotmail.com>
2017-03-06 10:22:10 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,25 @@
+cdn
+======
+
+> cdn - [nodejs](http://nodejs.org) library
+
+## Install
+
+    $ npm install cdn
+
+## Usage
+
+## Development
+
+### Install dependencies
+
+    $ make install
+
+### Run tests
+
+    $ make test
+
+## License
+
+(The MIT License)
+see LICENSE file for details...
---
+++ test/lib/cdn.test.js
@@ -0,0 +1,19 @@
+
+var should = require("chai").should(),
+    suite = require("../suite"),
+    cdn = require(suite.path("cdn"));
+
+describe("cdn", function(){
+
+    beforeEach(function() {
+
+    });
+
+    describe("", function() {
+
+        it("", function() {
+
+        });
+    });
+
+});
